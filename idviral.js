8b5ac1ab3dcc00a2ae28ff2c297405aab4fab608
Hasim Fadil Alatas <35094236+cakfadil@users.noreply.github.com>
2019-04-12 15:40:41 +0700

    Update idviral.js

--- idviral.js
+++ idviral.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "URL HALAMAN?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://www.idviral.site/p/about.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "n";
